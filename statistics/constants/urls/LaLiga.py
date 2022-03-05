8323eac76ddeece6e92d64d897c1f10ff22f3102
jke94 <jcarracedo.dev@gmail.com>
2022-03-05 15:13:16 +0100

    Add urls.

---
+++ statistics/constants/urls/LaLiga.py
@@ -0,0 +1,64 @@
+
+url_LaLiga_seasson_92_93 = "https://fbref.com/en/comps/12/1022/schedule/1992-1993-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_93_94 = "https://fbref.com/en/comps/12/1021/schedule/1993-1994-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_94_95 = "https://fbref.com/en/comps/12/1020/schedule/1994-1995-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_95_96 = "https://fbref.com/en/comps/12/1019/schedule/1995-1996-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_96_97 = "https://fbref.com/en/comps/12/1018/schedule/1996-1997-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_97_98 = "https://fbref.com/en/comps/12/1017/schedule/1997-1998-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_98_99 = "https://fbref.com/en/comps/12/1016/schedule/1998-1999-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_99_00 = "https://fbref.com/en/comps/12/42/schedule/1999-2000-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_00_01 = "https://fbref.com/en/comps/12/53/schedule/2000-2001-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_01_02 = "https://fbref.com/en/comps/12/70/schedule/2001-2002-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_02_03 = "https://fbref.com/en/comps/12/93/schedule/2002-2003-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_03_04 = "https://fbref.com/en/comps/12/124/schedule/2003-2004-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_04_05 = "https://fbref.com/en/comps/12/158/schedule/2004-2005-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_05_06 = "https://fbref.com/en/comps/12/196/schedule/2005-2006-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_06_07 = "https://fbref.com/en/comps/12/245/schedule/2006-2007-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_07_08 = "https://fbref.com/en/comps/12/299/schedule/2007-2008-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_08_09 = "https://fbref.com/en/comps/12/355/schedule/2008-2009-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_09_10 = "https://fbref.com/en/comps/12/422/schedule/2009-2010-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_10_11 = "https://fbref.com/en/comps/12/488/schedule/2010-2011-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_11_12 = "https://fbref.com/en/comps/12/557/schedule/2011-2012-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_12_13 = "https://fbref.com/en/comps/12/624/schedule/2012-2013-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_13_14 = "https://fbref.com/en/comps/12/690/schedule/2013-2014-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_14_15 = "https://fbref.com/en/comps/12/755/schedule/2014-2015-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_15_16 = "https://fbref.com/en/comps/12/1488/schedule/2015-2016-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_16_17 = "https://fbref.com/en/comps/12/1547/schedule/2016-2017-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_17_18 = "https://fbref.com/en/comps/12/1652/schedule/2017-2018-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_18_19 = "https://fbref.com/en/comps/12/1886/schedule/2018-2019-La-Liga-Scores-and-Fixtures"
+url_LaLiga_seasson_19_20 = "https://fbref.com/en/comps/12/3239/schedule/2019-2020-La-Liga-Scores-and-Fixtures",
+url_LaLiga_seasson_20_21 = "https://fbref.com/en/comps/12/10731/schedule/2020-2021-La-Liga-Scores-and-Fixtures",
+url_LaLiga_seasson_21_22 = "https://fbref.com/en/comps/12/schedule/La-Liga-Scores-and-Fixtures"
+
+urls_LaLiga_seasons = [
+    "".join(url_LaLiga_seasson_92_93),
+    "".join(url_LaLiga_seasson_93_94),
+    "".join(url_LaLiga_seasson_94_95),
+    "".join(url_LaLiga_seasson_95_96),
+    "".join(url_LaLiga_seasson_96_97),
+    "".join(url_LaLiga_seasson_97_98),
+    "".join(url_LaLiga_seasson_98_99),
+    "".join(url_LaLiga_seasson_99_00),
+    "".join(url_LaLiga_seasson_00_01),
+    "".join(url_LaLiga_seasson_01_02),
+    "".join(url_LaLiga_seasson_02_03),
+    "".join(url_LaLiga_seasson_03_04),
+    "".join(url_LaLiga_seasson_04_05),
+    "".join(url_LaLiga_seasson_05_06),
+    "".join(url_LaLiga_seasson_06_07),
+    "".join(url_LaLiga_seasson_07_08),
+    "".join(url_LaLiga_seasson_08_09),
+    "".join(url_LaLiga_seasson_09_10),
+    "".join(url_LaLiga_seasson_10_11),
+    "".join(url_LaLiga_seasson_11_12),
+    "".join(url_LaLiga_seasson_12_13),
+    "".join(url_LaLiga_seasson_13_14),
+    "".join(url_LaLiga_seasson_14_15),
+    "".join(url_LaLiga_seasson_15_16),
+    "".join(url_LaLiga_seasson_16_17),
+    "".join(url_LaLiga_seasson_17_18),
+    "".join(url_LaLiga_seasson_18_19),
+    "".join(url_LaLiga_seasson_19_20),
+    "".join(url_LaLiga_seasson_20_21),
+    "".join(url_LaLiga_seasson_21_22)
+        ]
---
+++ statistics/constants/urls/PremiereLeague.py
@@ -0,0 +1,40 @@
+
+url_PremiereLeague_seasson_92_93 = ""
+url_PremiereLeague_seasson_93_94 = ""
+url_PremiereLeague_seasson_94_95 = ""
+url_PremiereLeague_seasson_95_96 = ""
+url_PremiereLeague_seasson_96_97 = ""
+url_PremiereLeague_seasson_97_98 = ""
+url_PremiereLeague_seasson_98_99 = ""
+url_PremiereLeague_seasson_99_00 = ""
+url_PremiereLeague_seasson_00_01 = ""
+url_PremiereLeague_seasson_01_02 = ""
+url_PremiereLeague_seasson_02_03 = ""
+url_PremiereLeague_seasson_03_04 = ""
+url_PremiereLeague_seasson_04_05 = ""
+url_PremiereLeague_seasson_05_06 = ""
+url_PremiereLeague_seasson_06_07 = ""
+url_PremiereLeague_seasson_07_08 = ""
+url_PremiereLeague_seasson_08_09 = ""
+url_PremiereLeague_seasson_09_10 = ""
+url_PremiereLeague_seasson_10_11 = ""
+url_PremiereLeague_seasson_11_12 = ""
+url_PremiereLeague_seasson_12_13 = ""
+url_PremiereLeague_seasson_13_14 = ""
+url_PremiereLeague_seasson_14_15 = ""
+url_PremiereLeague_seasson_15_16 = ""
+url_PremiereLeague_seasson_16_17 = "https://fbref.com/en/comps/9/1526/schedule/2016-2017-Premier-League-Scores-and-Fixtures"
+url_PremiereLeague_seasson_17_18 = "https://fbref.com/en/comps/9/1631/schedule/2017-2018-Premier-League-Scores-and-Fixtures"
+url_PremiereLeague_seasson_18_19 = "https://fbref.com/en/comps/9/1889/schedule/2018-2019-Premier-League-Scores-and-Fixtures"
+url_PremiereLeague_seasson_19_20 = "https://fbref.com/en/comps/9/3232/schedule/2019-2020-Premier-League-Scores-and-Fixtures",
+url_PremiereLeague_seasson_20_21 = "https://fbref.com/en/comps/9/10728/schedule/2020-2021-Premier-League-Scores-and-Fixtures",
+url_PremiereLeague_seasson_21_22 = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
+
+urls_PremiereLeague_seasons = [
+    "".join(url_PremiereLeague_seasson_16_17),
+    "".join(url_PremiereLeague_seasson_17_18),
+    "".join(url_PremiereLeague_seasson_18_19),
+    "".join(url_PremiereLeague_seasson_19_20),
+    "".join(url_PremiereLeague_seasson_20_21),
+    "".join(url_PremiereLeague_seasson_21_22)
+        ]
